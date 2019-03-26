3fc9853aa79e3a6885c1fbc3c02eb59e4f800171
Brian McCarthy <brianmccarthy95@gmail.com>
2019-03-26 23:09:48 +0000

    Changed parser to add bool flags

--- scripts/extract_raw_data.py
+++ scripts/extract_raw_data.py
@@ -27,6 +27,15 @@ def extract_results(args):
     os.chdir(orig_loc)
 
 
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
 def main():
 
     parent_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
@@ -36,8 +45,8 @@ def main():
     parser = argparse.ArgumentParser(description='Retrieve results from simulation and store to raw_data')
     parser.add_argument("-r", "--results-folder", help="Results folder")
     parser.add_argument("-o", "--output-folder", help="Folder to place raw data in", default=raw_data_folder)
-    parser.add_argument("-s", "--scalars", help="Retrieve scalar results", default=True)
-    parser.add_argument("-v", "--vectors", help="Retrieve vector results", default=True)
+    parser.add_argument("-s", "--scalars", help="Retrieve scalar results", type=str2bool, default=True)
+    parser.add_argument("-v", "--vectors", help="Retrieve vector results", type=str2bool, default=True)
 
     args = parser.parse_args()
 
