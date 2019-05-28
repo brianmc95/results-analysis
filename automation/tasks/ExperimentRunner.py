cce3d6b5b4a1bf4a7fa7d7ada3f536fd1cc9d145
Brian McCarthy <brianmccarthy95@gmail.com>
2019-05-28 11:55:07 +0100

    Experiment runner updated to ensure it changes configurations as it goes, also adapted how it reports result files (full path now)

--- automation/tasks/ExperimentRunner.py
+++ automation/tasks/ExperimentRunner.py
@@ -1,8 +1,8 @@
 import multiprocessing
 import logging
+import configparser
 from subprocess import Popen, PIPE, STDOUT
 import time
-import datetime
 import os
 import re
 
@@ -13,44 +13,44 @@ class ExperimentRunner:
         self.processors = multiprocessing.cpu_count()
         self.config = config
         self.experiment_type = experiment_type
+
+        # Setup config parser to change experiment parameters.
+        self.ini_file = self.config["omnetpp_ini"]
+        self.configParser = configparser.ConfigParser()
+        self.configParser.optionxform = str
+        self.configParser.read(self.ini_file)
+
         self.logger = logging.getLogger("ExperimentRunner")
 
-    def start_experiment(self):
+    def start_experiment(self, now):
 
         result_dirs = []
-        now = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
 
         for config in self.config["config_names"]:
 
-            if self.config["config_names"][config] <= 0:
+            if self.config["config_names"][config]["repeat"] <= 0:
                 continue
 
             output_data_path = "{}/data/omnet/{}/{}-{}".format(os.getcwd(), self.experiment_type, config, now)
 
             os.mkdir(output_data_path)
 
-            result_dirs.append(os.path.basename(os.path.normpath(output_data_path)))
-
             num_processes = self.config["parallel_processes"]
             if num_processes > self.processors:
                 self.logger.warn("Too many processes, going to revert to total - 1")
                 num_processes = self.processors - 1
 
-            self.logger.info("Beginning simulation of config: {}, total of {} runs of this configuration".format(config, self.config["config_names"][config]))
+            self.logger.info("Beginning simulation of config: {}, total of {} runs of this configuration"
+                             .format(config, self.config["config_names"][config]))
 
             configs = []
-            for _ in range(self.config["config_names"][config]):
+            for _ in range(self.config["config_names"][config]["repeat"]):
                 configs.append(config)
 
             self.logger.debug("Configurations list: {}".format(configs))
-            number_of_batches = len(configs)//num_processes
+            number_of_batches = len(configs) // num_processes
             if number_of_batches == 0:
                 number_of_batches = 1
-
-            if config not in self.config["config_names"]:
-                self.logger.error(
-                    "Config: {} does not exist in config files".format(config))
-                raise Exception("Config: {} does not exist in config files".format(config))
 
             orig_loc = os.getcwd()
 
@@ -77,28 +77,47 @@ class ExperimentRunner:
 
             self.logger.info("Completed scenario setup")
 
-            i = 0
-            while i < len(configs):
-                if len(configs) < num_processes:
-                    num_processes = len(configs)
+            conf_count = 1
+            named = False
+            if "naming" in self.config["config_names"][config]:
+                # This simulation has multiple different runs, so need to update each.
+                conf_count = len(self.config["config_names"][config]["naming"])
+                self.logger.info("Number of sub-configurations of {} is {}".format(config, conf_count))
+                named = True
 
-                self.logger.info("Starting up processes, batch {}/{} of {} processes".format((i//num_processes)+1,
-                                                                                             number_of_batches,
-                                                                                             num_processes))
-                pool = multiprocessing.Pool(processes=num_processes)
+            current_conf = 0
 
-                pool.map(self.run_experiment, list(range(num_processes)))
+            while current_conf < conf_count:
 
-                self.logger.info("Batch {}/{} complete".format((i // num_processes) + 1, number_of_batches))
+                self.update_config(config, current_conf)
 
-                self.store_results(config, i, now)
+                i = 0
+                while i < len(configs):
+                    if len(configs) < num_processes:
+                        num_processes = len(configs)
 
-                i += num_processes
+                    self.logger.info("Starting up processes, batch {}/{} of {} processes".format((i//num_processes)+1,
+                                                                                                 number_of_batches,
+                                                                                                 num_processes))
+                    pool = multiprocessing.Pool(processes=num_processes)
 
-            self.logger.info("Removing {} dir from results".format(config))
+                    pool.map(self.run_experiment, list(range(num_processes)))
 
-            old_output = "{}/data/omnet/{}/{}".format(os.getcwd(), self.experiment_type, config)
-            os.removedirs(old_output)
+                    self.logger.info("Batch {}/{} complete".format((i // num_processes) + 1, number_of_batches))
+
+                    if named:
+                        result_dirs.append(self.store_results(config, i, now, self.config["config_names"][config]["naming"][current_conf]))
+                    else:
+                        result_dirs.append(self.store_results(config, i, now))
+
+                    i += num_processes
+
+                self.logger.info("Removing {} dir from results".format(config))
+
+                old_output = "{}/data/omnet/{}/{}".format(os.getcwd(), self.experiment_type, config)
+                os.removedirs(old_output)
+
+                current_conf += 1
 
         self.logger.info(result_dirs)
         return result_dirs
@@ -139,7 +158,7 @@ class ExperimentRunner:
 
         self.logger.debug("Moved backed to original location {}".format(os.getcwd()))
 
-    def store_results(self, config, runs_so_far, now):
+    def store_results(self, config, runs_so_far, now, name=None):
         self.logger.info("Moving results for config {} to a known location".format(config))
         self.logger.info("Current directory {}".format(os.getcwd()))
 
@@ -153,27 +172,63 @@ class ExperimentRunner:
         result_files.sort()
 
         for result_file in result_files:
+
             self.logger.debug(re.findall(pattern, result_file)[0])
+
             if previous_process != re.findall(pattern, result_file)[0]:
                 previous_process = re.findall(pattern, result_file)[0]
                 run_num += 1
+
                 self.logger.debug("Moving onto next run {}".format(run_num))
 
             extension = os.path.splitext(result_file)[1]
-            new_loc = "{}/{}/{}-{}/run-{}{}".format(output_data_path, self.experiment_type,
-                                                    config, now, run_num, extension)
+
+            if name:
+                new_dir = "{}/{}/{}-{}".format(output_data_path, self.experiment_type, name, now)
+                new_loc = "{}/run-{}{}".format(new_dir, run_num, extension)
+                if not os.path.isdir(new_dir):
+                    os.mkdir(new_dir)
+
+            else:
+                new_dir = "{}/{}/{}-{}".format(output_data_path, self.experiment_type, config, now)
+                new_loc = "{}/run-{}{}".format(new_dir, run_num, extension)
+                if not os.path.isdir(new_dir):
+                    os.mkdir(new_dir)
+
             result_file = "{}/{}/{}/{}".format(output_data_path, self.experiment_type, config, result_file)
 
             os.rename(result_file, new_loc)
             self.logger.debug("Moved file {} to {}".format(result_file, new_loc))
 
+        return os.path.dirname(new_loc)
+
     def update_config(self, config_name, config_variant):
         """
         Allow for th changing of a config to a different configuration of the same type.
         e.g. change probability of resourceKeep across multiple simulations.
-        :param config_name:
-        :param config_variant:
-        :return:
+        :param config_name: Name of the ini file configuration to run.
+        :param config_variant: Variant of the configuration to use, index in list.
         """
-        # TODO: Actually implement this as it is a bit of a extended project.
-        return
+        self.logger.info("Updating Config {} of omnetpp.ini file in loc: {}".format(config_name, self.ini_file))
+
+        omnet_conf_name = None
+
+        for omnet_name in self.configParser.sections():
+            if config_name.lower() in omnet_name.lower():
+                omnet_conf_name = omnet_name
+
+        for update_param in self.config["config_names"][config_name]["params"]:
+            self.logger.debug("Param to upate the config {}".format(update_param))
+            for param in self.configParser[omnet_conf_name]:
+                self.logger.debug("param in config {}".format(param))
+
+                if update_param.lower() in param.lower():
+                    updated_value = self.config["config_names"][config_name]["params"][update_param][config_variant]
+                    self.logger.info("Updating {} with value {}".format(param, updated_value))
+                    self.configParser[omnet_conf_name][param] = str(updated_value)
+
+                    # No need to continue searching
+                    continue
+
+        with open(self.ini_file, "w") as omnetpp_ini:
+            self.configParser.write(omnetpp_ini)
